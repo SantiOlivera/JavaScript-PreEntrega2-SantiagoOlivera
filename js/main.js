da6bf9f5cbfeedef855df0c187e1a67b9e1d41f8
SantiOlivera <santioli1313@gmail.com>
2025-08-14 09:54:39 -0300

    terminado

--- js/main.js
+++ js/main.js
@@ -1,91 +1,85 @@
-alert("¡Bienvenido al sistema de reservas!")
+const canchas = [
+    {
+        id: 1,
+        tipo: "F5",
+        horarios: ["16:00", "17:00", "20:00", "21:00"]
+    },
+    {
+        id: 2,
+        tipo: "F7",
+        horarios: ["18:00","19:00", "22:00", "23:00"]
+    },
+]
 
-function mostrarReserva (dato1, dato2, dato3, dato4, dato5, dato6) {
-    console.log("RESERVA:")
-    console.log("Nombre y apellido: "+dato1+" - DNI: "+dato2+" - Día: "+dato3+" - Hora: "+dato4+":00hs"+" - Tipo de cancha: "+dato5+" - Precio final: $"+dato6)
-    alert("RESERVA REALIZADA CON EXITO")
+let reservas = JSON.parse(localStorage.getItem("reservas")) || []
+
+
+let mostrarHorarios = document.getElementById("horarios")
+let listado = document.getElementById("listado")
+let botonReservar = document.getElementById("botonReservar")
+let botonBorrarReservas = document.getElementById("botonBorrarReservas")
+
+
+function renderCanchas(horarioCanchas) {
+    mostrarHorarios.innerHTML = ""
+    horarioCanchas.forEach(cancha => {
+        const carta = document.createElement("div")
+        carta.innerHTML = `<h3>${cancha.tipo}</h3>
+                           <p>${cancha.horarios.join(" - ")} horas</p>`
+        mostrarHorarios.appendChild(carta)
+    })
 }
 
-function descuento (precios) {
-    let resultado = precios - (10 * precios / 100)
+renderCanchas(canchas)
 
-    return resultado
+
+function mostrarReservas () {
+    listado.innerHTML = ""
+    reservas.forEach (reserva => {
+        const div = document.createElement("div")
+        div.innerHTML = `<li>Nombre y Apellido: ${reserva.nombre}</li>
+                         <li>Telefono: ${reserva.telefono}</li>
+                         <li>Tipo: ${reserva.tipoCancha}</li>
+                         <li>Fecha: ${reserva.fecha}</li>
+                         <li>Hora: ${reserva.hora}</li>`
+        listado.appendChild(div)
+    })
 }
 
-function mensajeGracias (usuario) {
-    alert("¡Gracias por tu reserva, "+usuario+"!")
+
+botonReservar.addEventListener("click", () => {
+    let nombre = document.getElementById("nombre").value
+    let telefono = document.getElementById("telefono").value
+    let tipoCancha = document.getElementById("tipoCancha").value
+    let fecha = document.getElementById("fecha").value
+    let hora = document.getElementById("hora").value
+
+    reservas.push({nombre, telefono, tipoCancha, fecha, hora})
+
+    localStorage.setItem("reservas", JSON.stringify(reservas))
+
+    horariosActualizados(tipoCancha, hora)
+
+    renderCanchas(canchas)
+    mostrarReservas()
+})
+
+
+
+function horariosActualizados(tipo, hora) {
+    canchas.forEach(cancha => {
+        if (cancha.tipo == tipo) {
+            cancha.horarios = cancha.horarios.filter(horario => horario != hora)
+        }
+    })
 }
 
 
-let turno = prompt("Quiere reservar un turno? (si/no)").toLowerCase()
-while (turno == "si") {
-    // Les di un valor al principio porque si los declaro dentro de los ifs no se guardan los valores
-    let hora = 0
-    let tipo = ""
-    let precio = 0
-    let total = 0
-
-    const horariosF5 = ["16hs", "17hs", "20hs", "21hs"]
-    const horariosF7 = ["18hs", "19hs", "22hs", "23hs"]
-
-    alert("Ingrese los siguientes datos")
-    let nya = prompt("Nombre y apellido:")
-    let dni = parseInt(prompt("DNI:"))
-    let dia = parseInt(prompt("Día:"))
-
-    console.log("Precios: Hasta las 20hs el precio es de $22.000 - De las 20hs en adelante el precio es de $32.000")
-    alert("ATENCION: ¡LOS TURNOS A PARTIR DE LAS 20HS (INCLUSIVE) TIENEN UN 10% DE DESCUENTO!")
+mostrarReservas()
 
 
-    console.log("1.Futbol 5 / 2.Futbol 7")
-    let numCanchas = parseInt(prompt("¿Qué tipo de cancha querés reservar? (1/2)"))
-    while (numCanchas != 1 && numCanchas != 2) {
-        alert("Error al ingresar el numero")
-        numCanchas = parseInt(prompt("¿Qué tipo de cancha querés reservar? (1/2)"))
-    }
-
-
-    if (numCanchas == 1) {
-        console.log("Horarios disponibles:")
-        for (const cancha of horariosF5){
-            console.log(cancha)
-        }
-        
-        hora = parseInt(prompt("Ingrese el horario que quiere reservar (ej: '16')"))
-        while (hora != 16 && hora != 17 && hora != 20 && hora != 21) {
-            alert("Horario inválido. Los horarios disponibles son: 16, 17, 20, 21")
-            hora = parseInt(prompt("Ingrese el horario que quiere reservar (ej: '16')"))
-        }
-
-        tipo = "Futbol 5"
-
-    } else {
-        console.log("Horarios disponibles:")
-        for (const cancha of horariosF7) {
-            console.log(cancha)
-        }
-
-        hora = parseInt(prompt("Ingrese el horario que quiere reservar (ej: '18')"))
-        while (hora != 18 && hora != 19 && hora != 22 && hora != 23) {
-            alert("Horario inválido. Los horarios disponibles son: 18, 19, 22, 23")
-            hora = parseInt(prompt("Ingrese el horario que quiere reservar (ej: '18')"))
-        }
-
-        tipo = "Futbol 7"
-    }
-    
-
-    if (hora >= 16 && hora < 20) {
-       total = 22000
-
-    } else if (hora >= 20) {
-        precio = 32000
-
-        total = descuento(precio)
-    }
-
-    mostrarReserva (nya, dni, dia, hora, tipo, total)
-    mensajeGracias (nya)
-
-    turno = prompt("Quiere reservar otro turno? (si/no)").toLowerCase()
-}
+botonBorrarReservas.addEventListener ("click", () => {
+    reservas = []
+    localStorage.removeItem("reservas")
+    listado.innerHTML = ""
+})
